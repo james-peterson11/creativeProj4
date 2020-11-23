135def882267837e5ca014f2387e66ca911e9b9f
james-peterson11 <jamesp11>
2020-11-23 22:48:22 +0000

    test

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,12 +1,100 @@
 <template>
 <div class="home">
+  <section class="image-gallery">
+    <div class="image" v-for="item in items" :key="item.id">
+      <h2>{{item.title}}</h2>
+      <img :src="item.path" />
+    </div>
+  </section>
+  <div id="footer">
+      <p>Page Author: James Peterson</p>
+      <p><a href="https://github.com/BYU-CS-260-Winter-2020/lab-4-museum-of-ordinary-objects-james-peterson11.git">GitHub Repository</a></p>
+  </div>
 </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import axios from 'axios';
 export default {
   name: 'Home',
+  data() {
+    return {
+     items: [],
+    }
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    async getItems() {
+      try {
+        let response = await axios.get("/api/items");
+        this.items = response.data;
+        return true;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  }
 }
 </script>
+
+<style scoped>
+.image h2 {
+  font-style: italic;
+}
+
+/* Masonry */
+*,
+*:before,
+*:after {
+  box-sizing: inherit;
+}
+
+.image-gallery {
+  column-gap: 1.5em;
+}
+
+.image {
+  margin: 0 0 1.5em;
+  display: inline-block;
+  width: 100%;
+}
+
+.image img {
+  width: 100%;
+}
+
+#footer {
+  display: flex;
+  flex-shrink: 0;
+  height: 50px;
+  width: 100%;
+  color: #020E14;
+  flex-direction: row;
+  justify-content: space-around;
+  background-color: #D3BEB5;
+}
+
+/* Masonry on large screens */
+@media only screen and (min-width: 1024px) {
+  .image-gallery {
+    column-count: 4;
+  }
+}
+
+/* Masonry on medium-sized screens */
+@media only screen and (max-width: 1023px) and (min-width: 768px) {
+  .image-gallery {
+    column-count: 3;
+  }
+}
+
+/* Masonry on small screens */
+@media only screen and (max-width: 767px) and (min-width: 540px) {
+  .image-gallery {
+    column-count: 2;
+  }
+}
+</style>
